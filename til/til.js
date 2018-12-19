275c576e273249c278ba9f16e398726777b85442
TravCav <xurrux@gmail.com>
2018-12-19 14:48:27 -0500

    lolwut

--- til/til.js
+++ til/til.js
@@ -1,42 +1,142 @@
 const fs = require('fs'); // so we can read and write files
-let char = '';
-let token = '';
 let tempToken = '';
-var data = fs.readFileSync('app.til', { encoding: 'utf8' });
+let readIndex = 0;
+let program = {
+    variables: [],
+    tokens: []
+};
+
+var data = fs.readFileSync('app.til', {
+    encoding: 'utf8'
+}) + "\r\n";
 
 console.log("---Start File---");
 console.log(data);
 console.log("---End File---");
 
-function Reader(){
-    let readIndex = 0;
-    return function () {
-        return data[readIndex++];
+function CheckForToken(possibleToken) {
+    let foundToken = {};
+    switch (possibleToken) {
+        case "yodawg":
+            let comment = TheRestofTheLine();
+            foundToken = {
+                tokenType: 'comment',
+                value: comment
+            };
+            break;
+        case "wordstuff":
+            foundToken = MakeVariable('string');
+            break;
+        case "mathstuff":
+            foundToken = MakeVariable('number');
+            break;
+        case "peepthis":
+            foundToken = OutputStuff();
+            break;
+        default:
+            foundToken = {
+                tokenType: 'unknown',
+                value: possibleToken
+            };
+            break;
+    }
+
+    console.log(foundToken);
+    return foundToken;
+}
+
+function GetVariable(varName) {
+    let foundVar = null;
+    for (let index = 0; index < program.variables.length; index++) {
+        let element = program.variables[index];
+        if (element.name === varName ) {
+            return element;
+        }
+    }
+
+    return foundVar;
+}
+
+function MakeVariable(variableType) {
+    let varName = NextWord();
+    let varValue = TheRestofTheLine();
+    const varObject = {
+        tokenType: 'variable',
+        varType: variableType,
+        name: varName,
+        value: varValue
     };
+
+    let varFound = false;
+    for (let index = 0; index < program.variables.length; index++) {
+        let element = program.variables[index];
+        if (element.name === varName ) {
+            program.variables[index] = varObject;
+            varFound = true;
+        }
+    }
+
+    if(!varFound) {
+        program.variables.push(varObject);
+    }
+  
+    return varObject;
 }
 
-let read = Reader();
 
-function GetChar(){
-    char = read();
-    return char;
+function NextWord() {
+    let scanChar = "";
+    let word = "";
+    do {
+        word += scanChar;
+        scanChar = Read();
+    } while (scanChar != " " && scanChar != "\r" && scanChar != "\n");
+
+    return word;
 }
 
-function FindToken() {
-    GetChar();
-    tempToken += char;
-     if (tempToken == '.assembly ') {
-         console.log('found assembly');
-         tempToken = '';
-         const nameToken = FindToken();
-         return { type: 'assembly', name: nameToken };
-     } else 
-    if (char == ' ') {
-        return tempToken.trim();
-    } else {
-        console.log('still just: ' + tempToken);
-        return FindToken();
+function OutputStuff() {
+    const varName = TheRestofTheLine();
+    const outputVariable = GetVariable(varName);
+
+    outputToken = { type: 'output', outVar: outputVariable };
+
+    return outputToken;
+}
+
+function Read() {
+    return data[readIndex++];
+}
+
+function TheRestofTheLine() {
+    let scanChar = "";
+    let lineContents = "";
+    while (scanChar != "\r" && scanChar != "\n") {
+        lineContents += scanChar;
+        scanChar = Read();
+    }
+
+    return lineContents;
+}
+
+while (readIndex < data.length) {
+    let token = "";
+    let currentChar = Read();
+    //console.log(currentChar);
+    switch (currentChar) {
+        case " ":
+        case "\r":
+        case "\n":
+            if (tempToken > '') {
+                token = CheckForToken(tempToken);
+                program.tokens.push(token);
+                tempToken = "";
+            }
+            break;
+        default:
+            tempToken += currentChar;
+            break;
     }
 }
 
-console.log(FindToken());
+console.log(program);
